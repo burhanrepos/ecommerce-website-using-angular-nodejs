743e8fb86ed0ef4b71fbc01ad06ea40568fd3a0c
Burhan Shaheen <hafiz.burhan834@gmail.com>
2023-03-07 22:26:16 +0500

    added login apis

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { sample_foods, sample_tags } from './data';
-
+import { sample_foods, sample_tags, sample_users } from './data';
+import jwt from 'jsonwebtoken';
 const app = express();
+app.use(express.json())
 
-//localhost:5000
 
 app.use(cors({
     credentials: true,
@@ -24,6 +24,30 @@ app.get('/api/foods/tag/:tagName', (req,res) =>{
     const foods = sample_foods.filter(food => food.tags?.includes(tagName))
     res.send(foods)
 })
+
+app.post("/api/users/login",(req,res)=>{
+    const {email, password} = req.body;
+
+    const user = sample_users.find(user => user.email === email && user.password === password);
+
+    if(user){
+        res.send(generateTokenResponse(user))
+    }else{
+        res.status(400).send("User name or password is not valid!");
+    }
+
+})
+
+const generateTokenResponse = (user:any) =>{
+    const token = jwt.sign({
+        email: user.email , isAdmin: user.isAdmin
+    },"SomeRandomText",{
+        expiresIn: '30d'
+    })
+
+    user.token = token;
+    return user;
+}
 
 // search term
 app.get("/api/foods/search/:searchTerm",(req,res)=>{
